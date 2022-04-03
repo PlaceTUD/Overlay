aed3fa2e389475248460a769f902a60b1e1c942f
Yanick <me@yanick.gay>
2022-04-04 01:55:42 +0200

    new overlay

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlaceTUD Overlay
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Overlay for PlaceTUD
 // @author       forked of oralekin (stoatified by Jeck0_0)
 // @match        https://hot-potato.reddit.com/embed*
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
                 Date.now = function() { return new Date().getTime(); }
             }
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/960311022911123527/960311158785585242/overlay.png";
+            i.src = "https://cdn.discordapp.com/attachments/960311022911123527/960325804582125629/overlay.png";
             i.style = "position: absolute;left: 0;top: 0px;image-rendering: pixelated;width: 2000px;height: 2000px;";
             i.id = "mcss-overlay"
             i.setAttribute("vis",1)
@@ -27,7 +27,7 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("button");
             i.style = "position: fixed;left: 10px; background: #fff; top: 50%;width: 50px;height: 50px;";
-            i.innerHTML = "T"
+            i.innerHTML = "X"
             i.onclick = function() {
                 const img = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.querySelector("#mcss-overlay")
                 if (img.getAttribute("vis") == "1") {
